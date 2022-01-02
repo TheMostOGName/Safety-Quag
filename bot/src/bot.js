15766f97b312ae8752d98b585c6d28d55142da69
Kenorbs <80411201+TheMostOGName@users.noreply.github.com>
2022-01-02 11:42:43 -0500

    Attempting to add Eliza

--- bot/src/bot.js
+++ bot/src/bot.js
@@ -272,6 +272,69 @@ let commands = {
                 }
             }
         }
+    },
+    "eliza": {
+        "desc": "Eliza is the world's first chatbot, created in 1965!",
+        "f": function(message, args) {
+            args.forEach(getInput); 
+            function getInput(input) {
+                if (userinput != undefined) {
+                    var userinput = `${userinput} ${input}`;
+                } else {
+                    var userinput = input;
+                }
+
+
+                var eliza = new ElizaBot();
+                var elizaLines = new Array();
+
+                var displayCols = 60;
+                var displayRows = 20;
+
+                function elizaReset() {
+                eliza.reset();
+                elizaLines.length = 0;
+                elizaStep();
+                }
+
+                function elizaStep() {
+                var f = document.forms.e_form;
+                // var userinput = f.e_input.value;
+                if (eliza.quit) {
+                f.e_input.value = '';
+                if (confirm("This session is over.\nStart over?")) elizaReset();
+                f.e_input.focus();
+                return;
+                }
+                else if (userinput != '') {
+                var usr = 'YOU:   ' + userinput;
+                var rpl ='ELIZA: ' + eliza.transform(userinput);
+                elizaLines.push(usr);
+                elizaLines.push(rpl);
+                // display nicely
+                // (fit to textarea with last line free - reserved for extra line caused by word wrap)
+                var temp  = new Array();
+                var l = 0;
+                for (var i=elizaLines.length-1; i>=0; i--) {
+                    l += 1 + Math.floor(elizaLines[i].length/displayCols);
+                    if (l >= displayRows) break
+                    else temp.push(elizaLines[i]);
+                }
+                elizaLines = temp.reverse();
+                f.e_display.value = elizaLines.join('\n');
+                }
+                else if (elizaLines.length == 0) {
+                // no input and no saved lines -> output initial
+                var initial = 'ELIZA: ' + eliza.getInitial();
+                elizaLines.push(initial);
+                f.e_display.value = initial + '\n';
+                }
+                f.e_input.value = '';
+                f.e_input.focus();
+                }
+            message.channel.send(rpl); 
+            }
+        }
     }
 } 
 
